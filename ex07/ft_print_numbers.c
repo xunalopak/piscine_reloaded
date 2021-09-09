dbc9e2b08ad622356b26df17dfbef9087c55f880
xuna <romainchampliau@gmail.com>
2021-09-10 01:50:21 +0200

    chocolatine au beurre, ex07

---
+++ ex07/ft_print_numbers.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_numbers(void)
+{
+	int	i;
+	
+	i = '0';
+	while (i != '9')
+	{
+		ft_putchar(i);
+		i++;
+	}
+	ft_putchar(i);
+}
